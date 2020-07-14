93a6b8e6e97f32dc38c40556c45a59ddf650bdd6
glowthem <glowing713@naver.com>
2020-07-14 18:43:25 +0900

    2020-07-14 ➕ displaying temperature

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
+// 대림1차아파트: 35.179105, 129.173659
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "d6d552c8ed7f923e20deff7fa2fe1642";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    console.log(data);
+    this.setState({ isLoading: false, temp: data.main.temp });
+  };
   getLocation = async () => {
     try {
-      state = {
-        isLoading: true,
-      };
       // 사용자 위치 정보 접근 허가 요청
       await Location.requestPermissionsAsync();
 
@@ -17,7 +29,8 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading: false });
+      // console.log(coords.latitude, coords.longitude);
+      this.getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Something's going wrong!!!", "Can't find your location!");
     }
@@ -27,7 +40,11 @@ export default class extends React.Component {
   }
 
   render() {
-    const isLoading = this.state;
-    return isLoading ? <Loading></Loading> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? (
+      <Loading></Loading>
+    ) : (
+      <Weather temp={Math.round(temp)}></Weather>
+    );
   }
 }
